e8fc32fa19638b0b2131fd9d8aa47fc5cb603950
manyasethi20 <160034294+manyasethi20@users.noreply.github.com>
2024-10-30 16:44:04 +0000

    const, let and var

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 54399250
+let accountEmail = "manya@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState;
+
+// accountId = 2
+
+accountEmail = "abc@gmail.com"
+accountPassword = "2145326"
+accountCity = "Bengaluru"
+
+console.log(accountId)
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
