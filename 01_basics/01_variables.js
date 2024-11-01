8894325144ef4b73dc825a43e8b8270ca55fcf23
manyasethi20 <160034294+manyasethi20@users.noreply.github.com>
2024-11-01 17:41:33 +0000

    variables

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -12,4 +12,8 @@ accountCity = "Bengaluru"
 
 console.log(accountId)
 
+/*
+Prefer not to use var because of issue in block scope and functional scope
+*/
+
 console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
