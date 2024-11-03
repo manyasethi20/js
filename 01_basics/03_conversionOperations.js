5b25d2bf419d6b3423977e83ab54aa512e0a86f2
manyasethi20 <160034294+manyasethi20@users.noreply.github.com>
2024-11-03 16:49:37 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,27 @@
+let score = "Manya"
+
+// console.log(typeof score)
+// console.log(typeof(score))
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+// "33" = 33
+// "33abc" = NaN
+// true = 1, false = 0
+
+let isLoggedIn = "Manya"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+// 0 = true, 1 = false
+// "" = false
+// "Manya" = true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
