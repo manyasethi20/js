870626984a0af210b0bfbacb88e4be55fed57337
manyasethi20 <160034294+manyasethi20@users.noreply.github.com>
2024-11-06 13:32:35 +0000

    math and num

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,34 @@
+const score = 100
+// console.log(score);
+
+const balance = new Number(200)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+const otherNumber = 9209.2393
+
+// console.log(otherNumber.toPrecision(5))
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'))
+
+//**************Maths**************** 
+
+// console.log(Math)
+// console.log(Math.abs(-3))
+// console.log(Math.round(5.73))
+// console.log(Math.floor(5.73))
+// console.log(Math.ceil(5.73))
+// console.log(Math.min(4, 3, 5, 7))
+// console.log(Math.max(4, 3, 5, 7))
+
+// console.log(Math.random())
+// console.log((Math.random()*10)+1)
+// console.log(Math.floor((Math.random()*10)+1))
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random()*(max - min +1)+min))
