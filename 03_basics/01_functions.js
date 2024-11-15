f0d6f5c9e96e6fba7d5e193fa50cc39d6041a996
manyasethi20 <160034294+manyasethi20@users.noreply.github.com>
2024-11-15 17:40:20 +0000

    functions with objects and array

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -34,4 +34,34 @@ function loginUserMessage(username = "Sam") {
 }
 
 // console.log(loginUserMessage("Manya"))
-console.log(loginUserMessage("Manya"))
+// console.log(loginUserMessage("Manya"))
+
+function calculateCartPrice(val1, val2, ...num1) { //... is rest here
+    return num1
+}
+
+// console.log(calculateCartPrice(200, 300, 390, 549))
+
+const user = {
+    username: "Manya",
+    price: 200000000
+}
+
+function handleObject(anyobject) {
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`)
+}
+
+// handleObject(user)
+// handleObject({
+//     username: "sam",
+//     price: 399
+// })
+
+const myNewArr = [200, 400, 600, 800]
+
+function returnSecondValue(getArray) {
+    return getArray[2]
+}
+
+// console.log(returnSecondValue(myNewArr))
+console.log(returnSecondValue([200, 300, 500, 400]))
