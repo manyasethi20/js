0e55da9705e91cf3d94e8ede230b79994b41afe1
manyasethi20 <160034294+manyasethi20@users.noreply.github.com>
2024-11-18 17:52:56 +0000

    execution context

---
+++ 03_basics/05_executionContext.js
@@ -0,0 +1,10 @@
+//javascript execution context
+
+//{} -> global (this) execution context
+
+// -> Global execution context
+// -> Function execution context
+// -> Eval execution context
+
+// {} -> Memory creation phase
+//    -> Execution phase
