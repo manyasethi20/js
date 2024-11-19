07cb3a49f35a45925f69b020709c13b6aa5ad00b
manyasethi20 <160034294+manyasethi20@users.noreply.github.com>
2024-11-19 17:52:11 +0000

    if-else

---
+++ 04_control_flow/one.js
@@ -0,0 +1,10 @@
+//if
+const isUserLoggedIn = true
+const temperature = 41
+
+if (temperature === 41) {
+    console.log("less than 50")
+} else {
+    console.log("temperature is greater than 50")
+}
+// <, >, <=, >=, !=, ===, !==
