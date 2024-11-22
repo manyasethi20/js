589b3fc8915354ab2f734e027222ac0ac3919610
manyasethi20 <160034294+manyasethi20@users.noreply.github.com>
2024-11-22 17:58:24 +0000

    while and do-while loops

---
+++ 05_iterations/two.js
@@ -0,0 +1,20 @@
+let index = 0;
+while(index <= 10) {
+    // console.log(`Value of index is ${index}`);
+    index += 2;
+}
+
+let myArray = ["batman", "flash", "spiderman"];
+
+let arr = 0;
+while(arr < myArray.length) {
+    // console.log(`Value is ${myArray[arr]}`);
+    arr += 1;
+}
+
+let score = 1;
+
+do {
+    console.log(`Score is ${score}`);
+    score += 1;
+} while(score <= 5)
